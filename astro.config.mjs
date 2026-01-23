79c646be038ed48dbf97ed561f16d4b1481e462b
Sandrine Mestas Gleizes <98587061+SuperSandrine@users.noreply.github.com>
2026-01-23 16:35:20 +0100

    fix pour passer le build dans astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,24 +4,23 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react'
 import markdoc from '@astrojs/markdoc'
 import keystatic from '@keystatic/astro'
-
-
 import tailwindcss from '@tailwindcss/vite';
-
-
 import netlify from '@astrojs/netlify';
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic()],  // dans la doc de astro: https://docs.astro.build/fr/guides/cms/keystatic/
+  site:'https://Laboucledelaboucle.netlify.app',
+  output: 'server',
+  integrations: [react(), markdoc()],  // dans la doc de astro: https://docs.astro.build/fr/guides/cms/keystatic/
   //  integrations: [react(), markdoc()],
+  adapter: netlify(),
 
   //static?
   // 12:05:01 PM: ! The output: "hybrid" option has been removed. Use output: "static" (the default) instead, which now behaves the same way.
 
   //output: 'server',
-  output: 'server',
+  //output: 'server',
 
   vite: {
     plugins: [tailwindcss()],
@@ -32,5 +31,5 @@ export default defineConfig({
     },
   },
 
-  adapter: netlify(),
+//  adapter: netlify(),
 });
